cf2bd9b734e2e9ece7e67f9d95584094927d22a4
atozzini <alexandretozzini@gmail.com>
2023-02-17 16:30:24 -0300

    add users

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,15 @@
 import * as restify from 'restify'
 import { environment } from '../common/environment'
+import { Router } from '../common/router'
 
 export class Server {
 
   application: restify.Server
 
-  initRoutes(): Promise<any> {
+  initRoutes(routers: Router[]): Promise<any> {
     return new Promise((resolve, reject) => {
       try {
+
         this.application = restify.createServer({
           name: 'meat-api',
           version: '1.0.0'
@@ -15,37 +17,14 @@ export class Server {
 
         this.application.use(restify.plugins.queryParser())
 
-        // routes
-
-        this.application.get('/info', [
-          (req, resp, next) => {
-            if (req.userAgent() && req.userAgent().includes('MSIE 7.0')) {
-              resp.status(400)
-              // resp.json({ message: 'Please, update your browser' })
-              let error = new Error()
-              error.statusCode = 400
-              error.message = 'Please, update your browser'
-
-              return next(error)
-            }
-          },
-          (req, resp, next) => {
-            resp.json({
-              browser: req.userAgent(),
-              method: req.method,
-              url: req.href(),
-              path: req.path(),
-              query: req.query
-            })
-            return next()
-          }])
+        //routes
+        for (let router of routers) {
+          router.applyRoutes(this.application)
+        }
 
         this.application.listen(environment.server.port, () => {
           resolve(this.application)
-          // console.log('API is running on http://localhost:3000')
         })
-
-        // this.application.on('error', (err))
 
       } catch (error) {
         reject(error)
@@ -53,7 +32,8 @@ export class Server {
     })
   }
 
-  bootstrap(): Promise<Server> {
-    return this.initRoutes().then(() => this)
+  bootstrap(routers: Router[] = []): Promise<Server> {
+    return this.initRoutes(routers).then(() => this)
   }
+
 }
